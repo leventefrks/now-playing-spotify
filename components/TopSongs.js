d16250ff84d1b239147c91cbe28ef65589f55147
leventefrks <levente.f@gmail.com>
2023-01-03 22:09:40 +0100

    top songs

--- components/TopSongs.js
+++ components/TopSongs.js
@@ -5,10 +5,7 @@ const TopSongs = () => {
   const { data, error, isLoading } = useSWR('/api/top-songs', fetcher);
 
   if (error) return <div className="text-xl">Failed to load</div>;
-
   if (isLoading) return <div className="text-xl">Loading...</div>;
-
-  console.log(data);
 
   return <div className="flex flex-col items-center">TopSongs</div>;
 };
