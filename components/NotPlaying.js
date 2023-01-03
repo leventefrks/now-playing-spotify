ad5a4f4a928d7d1bcfdf5254ab71ef680c9de45e
x-farkasle <x-farkasle@wizzair.com>
2023-01-03 19:20:43 +0100

    text mod

--- components/NotPlaying.js
+++ components/NotPlaying.js
@@ -5,8 +5,8 @@ const NotPlaying = () => {
     <h1 className="flex flex-col items-center gap-4 text-center text-2xl text-gray-100">
       <BsSpotify className="h-10 w-10 text-green-400" />
       <span className="font-light text-gray-50">
-        I&apos;m probably out there in the woods{' '}
-        <strong className="text-3xl">running</strong> or{' '}
+        I&apos;m probably out there{' '}
+        <strong className="text-3xl">running</strong>in the woods or{' '}
         <strong className="text-3xl">sleeping.</strong>😴
       </span>
     </h1>
